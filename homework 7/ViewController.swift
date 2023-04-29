da6ff60fc812df8180d06579bc6879290b9e4bb9
Michael Spain <m.spain@setonhill.edu>
2023-04-29 16:34:11 -0400

    finish

--- homework 7/ViewController.swift
+++ homework 7/ViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var outputLabel: UILabel!
+    
+    
+    @IBAction func tapFound(_ sender: Any) {
+        outputLabel.text = "You tapped!"
+        self.view.backgroundColor = UIColor.green
+    }
+    
+    
+    
+    @IBAction func swipeFound(_ sender: Any) {
+        outputLabel.text = "You swiped!"
+        self.view.backgroundColor = UIColor.yellow
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
